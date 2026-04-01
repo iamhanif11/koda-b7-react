974e852f93505c6bfb6b577d3100cc76970093d3
M. Hanif Irfan <mhirfan173@gmail.com>
2026-04-01 14:07:20 +0700

    style: fix write code

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <main className='p-10 flex gap-8 justify-center items-center'>
 
         <button
-        className='border-2 border-2 border-black bg-gray-300 w-5 h-5 rounded-md flex justify-center items-center font-bold'
+        className='border-2 border-black bg-gray-300 w-5 h-5 rounded-md flex justify-center items-center font-bold'
         onClick={() =>
         {if(count > 0){
           setCount((count) => count - 1)}
@@ -21,7 +21,7 @@ function App() {
         <h1 className='text-amber-600 text-2xl'>{count}</h1>
 
         <button
-          className="border-2 border-2 border-black bg-gray-300 w-5 h-5 rounded-md flex justify-center items-center font-bold"
+          className="border-2  border-black bg-gray-300 w-5 h-5 rounded-md flex justify-center items-center font-bold"
           onClick={() => 
             {if(count < 10){
             setCount((count) => count + 1)}
